7941a5d4f215ddb285b4dee854fe3fd4008e67cc
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-07-28 15:50:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import cv2
+from qrtools import QR 
+
+address=input("Enter location of qr image")
+
+my_QR = QR(filename = address) 
+  
+# decodes the QR code and returns True if successful 
+my_QR.decode() 
+  
+print(my_QR.data)
