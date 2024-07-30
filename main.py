f8bfe8c9fb16ab25804430996d249d726163f538
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-07-30 14:05:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-import cv2
+import cv2,device
 from qrtools import QR 
 
-address=input("Enter location of qr image")
 
-my_QR = QR(filename = address) 
+def get_data(img):
+  my_QR = QR(filename = address) 
+  # decodes the QR code and returns True if successful 
+  my_QR.decode() 
   
-# decodes the QR code and returns True if successful 
-my_QR.decode() 
+  print(my_QR.data)
+
+def get_list_camera():
+
+  List_cam={}
   
-print(my_QR.data)
+  # Get camera list
+  device_list = device.getDeviceList()
+  index = 0
+
+  
+  for camera in device_list:
+      List_cam[index]=camera
+      index += 1
+
+def creating_QR(data):
+  my_QR = QR(data) 
+  # encodes the QR code and returns True if successful 
+  my_QR.encode() 
